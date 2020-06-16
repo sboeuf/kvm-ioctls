a9223985588c1806f38b4c321a523d4be65cc279
Sebastien Boeuf <sebastien.boeuf@intel.com>
2020-06-16 16:45:34 +0200

    vcpu: Add KVM_KVMCLOCK_CTRL support
    The KVM_KVMCLOCK_CTRL lets the guest know that it has been paused, which
    will prevent from detecting soft lockups due to pausing and resuming
    operations.
    
    Signed-off-by: Sebastien Boeuf <sebastien.boeuf@intel.com>

--- src/ioctls/vcpu.rs
+++ src/ioctls/vcpu.rs
@@ -1018,6 +1018,30 @@ impl VcpuFd {
         Ok(reg_value)
     }
 
+    /// Notify the guest about the vCPU being paused.
+    ///
+    /// This signals to the host kernel that the specified guest is being paused by
+    /// userspace.  The host will set a flag in the pvclock structure that is checked
+    /// from the soft lockup watchdog.  The flag is part of the pvclock structure that
+    /// is shared between guest and host, specifically the second bit of the flags
+    /// field of the pvclock_vcpu_time_info structure.  It will be set exclusively by
+    /// the host and read/cleared exclusively by the guest.  The guest operation of
+    /// checking and clearing the flag must an atomic operation so
+    /// load-link/store-conditional, or equivalent must be used.  There are two cases
+    /// where the guest will clear the flag: when the soft lockup watchdog timer resets
+    /// itself or when a soft lockup is detected.  This ioctl can be called any time
+    /// after pausing the vcpu, but before it is resumed.
+    ///
+    pub fn kvmclock_ctrl(&self) -> Result<()> {
+        // Safe because we know that our file is a KVM fd and that the request
+        // is one of the ones defined by kernel.
+        let ret = unsafe { ioctl(self, KVM_KVMCLOCK_CTRL()) };
+        if ret != 0 {
+            return Err(errno::Error::last());
+        }
+        Ok(())
+    }
+
     /// Triggers the running of the current virtual CPU returning an exit reason.
     ///
     /// See documentation for `KVM_RUN`.
